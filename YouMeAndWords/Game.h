c1169ee02f2d969d9f006b6c334683b4e5d8e399
Raj Kandathi <rajkandathi@Rajs-MacBook-Pro.local>
2012-04-04 14:22:36 -0400

    Skelton file (with some hard coded data) that will represent a game.

---
+++ YouMeAndWords/Game.h
@@ -0,0 +1,15 @@
+//
+//  Game.h
+//  YouMeAndWords_v1
+//
+//  Created by Raj Kandati on 3/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Game : NSObject
+
++ (NSMutableDictionary *)getGameForGameNumber:(int)gameNumber;
+
+@end

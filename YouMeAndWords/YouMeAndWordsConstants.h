e429e5fa44c8d6763aa115fb1c147d7fe84992ac
Raj Kandathi <rajkandathi@Rajs-MacBook-Pro.local>
2012-04-04 13:50:11 -0400

    Constants file for the project.

---
+++ YouMeAndWords/YouMeAndWordsConstants.h
@@ -0,0 +1,22 @@
+//
+//  YouMeAndWordsConstants.h
+//  YouMeAndWords
+//
+//  Created by Raj Kandati on 3/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#define ALPHABET_WIDTH 50
+#define ALPHABET_HEIGHT 50
+#define NUMBERS_OF_COLUMNS_IN_GRID 3
+#define NUMBERS_OF_ROWS_IN_GRID 3
+#define BRODER_BETWEEN_CELLS_IN_GRID 10
+#define X_ORIGIN_OF_GRID 75
+#define Y_ORIGIN_OF_GRID 125
+#define SCORE_WIDTH 10
+#define SCORE_HEIGHT 10
+#define SCORE_X_ORIGIN ALPHABET_WIDTH - 10
+#define SCORE_Y_ORIGIN ALPHABET_HEIGHT - 10
+#define GAME_LETTERS_KEY @"GAME_LETTERS_KEY"
+#define GAME_WORDS_KEY @"GAME_WORDS_KEY"
+#define UUID_USER_DEFAULTS_KEY @"UUID"
